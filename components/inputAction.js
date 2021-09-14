3b7aa04cebacd54b8a06181f210d6d48ecae5baa
Ugur Armagan <ugurarmagan93@gmail.com>
2021-09-13 22:28:22 -0500

    single quotes and connecting to ethereum

--- components/inputAction.js
+++ components/inputAction.js
@@ -1,15 +1,15 @@
 export const InputActions = () => {
   return (
-    <div className="flex flex-col w-1/2 space-y-3">
+    <div className='flex flex-col w-1/2 space-y-3'>
       <input
-        type="text"
-        name="pokemon"
+        type='text'
+        name='pokemon'
         placeholder="What's Your Favorite Pokemon?"
-        className="text-center shadow-sm pt-0.5 mt-12 border-2 border-black rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full sm:text-md border-gray-300 rounded-lg"
+        className='text-center shadow-sm pt-0.5 mt-12 border-2 border-black rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full sm:text-md border-gray-300 rounded-lg'
       />
       <button
-        type="button"
-        className="inline-flex justify-center px-3 py-1.5 border border-transparent text-base font-semibold rounded shadow-sm text-white text-center tracking-wider bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-2 focus:ring-offset-2"
+        type='button'
+        className='inline-flex justify-center px-3 py-1.5 border border-transparent text-base font-semibold rounded shadow-sm text-white text-center tracking-wider bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-2 focus:ring-offset-2'
       >
         Add That Pokemon To The Blockchain
       </button>
--- components/layout.js
+++ components/layout.js
@@ -1,6 +1,6 @@
 export const Layout = (props) => {
   return (
-    <div className="max-w-4xl mx-auto sm:px-6 lg:px-8 flex flex-col items-center">
+    <div className='max-w-4xl mx-auto sm:px-6 lg:px-8 flex flex-col items-center'>
       {props.children}
     </div>
   );
