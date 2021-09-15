075cd26ee03253f83cebc8359923acabb5ae0473
Ugur Armagan <ugurarmagan93@gmail.com>
2021-09-15 13:22:30 -0500

    quick padding to header and logo

--- components/header.js
+++ components/header.js
@@ -2,7 +2,7 @@ import Image from 'next/image';
 import pokemonLogo from '../public/pokemon-logo.png';
 export const Header = () => {
   return (
-    <div className='flex flex-col items-center min-w-full space-y-4'>
+    <div className='flex flex-col items-center min-w-full space-y-8'>
       <h1 className='tracking-wide text-3xl lg:text-4xl font-pokemon'>
         Choose Your Favorite
       </h1>
